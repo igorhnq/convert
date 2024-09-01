6ce1bb1fca0d6ba1ce0280c07ee12eaf88011de5
igorhnq <igorhenrique102@gmail.com>
2024-09-01 17:33:08 -0300

    show result

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const form = document.querySelector("form");
 const currency = document.getElementById("currency");
 const amount = document.getElementById("amount");
 
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
+
 addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex, "");
@@ -16,17 +20,32 @@ form.addEventListener("submit", (event) => {
 
     switch(currency.value) {
         case "USD":
-            convertCurrency(currency.value, USD, "$USD");
+            convertCurrency(amount.value, USD, "$USD");
             break
         case "EUR":
-            convertCurrency(currency.value, EUR, "€");
+            convertCurrency(amount.value, EUR, "€");
             break
         case "GBP":
-            convertCurrency(currency.value, GBP, "£");
+            convertCurrency(amount.value, GBP, "£");
             break
     }
 })
 
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol);
+    try {
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+        footer.classList.add("show-result");
+        const total = formatCurrencyBRL(amount * price).replace("R$", "");
+        result.textContent = `${total} Reais`;
+    } catch (error) {
+        footer.classList.remove("show-result");
+        alert("Não foi possível converter!");  
+    }
+}
+
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
 }
